fedcdb4a126b361365b9de5480659dc3f4d5faca
r0mka <romanliveshere@gmail.com>
2020-11-13 02:31:27 -0800

    Complete initial implementation of User

--- 1-maps/src/User.ts
+++ 1-maps/src/User.ts
@@ -7,6 +7,10 @@ class User {
     lng: number;
   };
   constructor(name: string) {
-    this.name = name;
+    this.name = faker.name.firstName();
+    this.location = {
+      lat: parseFloat(faker.address.latitude()),
+      lng: parseFloat(faker.address.longitude()),
+    };
   }
 }
