2292649cbde456279a7afcf4f8fd6f5d0170b9be
r0mka <romanliveshere@gmail.com>
2020-11-18 04:52:39 -0800

    Make User and Company implement interface Mappable

--- 1-maps/src/Company.ts
+++ 1-maps/src/Company.ts
@@ -1,12 +1,13 @@
 import faker from 'faker';
-
-export class Company {
+import { Mappable } from './CustomMap';
+export class Company implements Mappable {
   companyName: string;
   catchPhrase: string;
   location: {
     lat: number;
     lng: number;
   };
+  color: string = 'red';
   constructor() {
     this.companyName = faker.company.companyName();
     this.catchPhrase = faker.company.catchPhrase();
--- 1-maps/src/CustomMap.ts
+++ 1-maps/src/CustomMap.ts
@@ -1,12 +1,10 @@
-import { User } from './User';
-import { Company } from './Company';
-
 export interface Mappable {
   location: {
     lat: number;
     lng: number;
   };
   markerContent(): string;
+  color: string;
 }
 
 export class CustomMap {
