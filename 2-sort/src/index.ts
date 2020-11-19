d1bd1096578168801bd1a46d48fcb3d5906d8be0
r0mka <romanliveshere@gmail.com>
2020-11-18 21:44:54 -0800

    Add bubble sort method to Sorter class

--- 2-sort/src/index.ts
+++ 2-sort/src/index.ts
@@ -1,7 +1,20 @@
-console.log('Hello Roman');
+class Sorter {
+  constructor(public collection: number[]) {}
 
-const logSomething = () => {
-  console.log('something else');
-};
+  sort(): void {
+    const { length } = this.collection;
+    for (let i = 0; i < length; i++) {
+      for (let j = 0; j < length - i - 1; j++) {
+        if (this.collection[j] > this.collection[j + 1]) {
+          const leftHand = this.collection[j];
+          this.collection[j] = this.collection[j + 1];
+          this.collection[j + 1] = leftHand;
+        }
+      }
+    }
+  }
+}
 
-logSomething();
+const sorter = new Sorter([10, 3, -5, 0]);
+sorter.sort();
+console.log(sorter.collection);
