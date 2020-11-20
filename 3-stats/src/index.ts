5c630f9aefbd17ad0a682c13ea676dc051e0bdb2
r0mka <romanliveshere@gmail.com>
2020-11-19 23:08:06 -0800

    Implement enums for matches

--- 3-stats/src/index.ts
+++ 3-stats/src/index.ts
@@ -1,1 +1,30 @@
-console.log('hello');
+import fs from 'fs';
+
+const matches = fs
+  .readFileSync('src/football.csv', {
+    encoding: 'utf-8',
+  })
+  .split('\n')
+  .map((row: string): string[] => row.split(','));
+
+// enum - enumeration
+enum MatchResult {
+  HomeWin = 'H',
+  AwayWin = 'A',
+  Draw = 'D',
+}
+
+let manUnitedWins = 0;
+
+for (let match of matches) {
+  if (match[1] === 'Man United' && match[5] === MatchResult.HomeWin) {
+    manUnitedWins++;
+  } else if (
+    match[2] === 'Man United' &&
+    match[5] === MatchResult.AwayWin
+  ) {
+    manUnitedWins++;
+  }
+}
+
+console.log(`Man United won ${manUnitedWins} games`);
