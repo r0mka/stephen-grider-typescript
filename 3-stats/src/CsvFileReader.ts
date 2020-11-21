85b5870cfb6c354504cbcf991dc8ccbeed57799b
r0mka <romanliveshere@gmail.com>
2020-11-21 01:33:45 -0800

    add utils

--- 3-stats/src/CsvFileReader.ts
+++ 3-stats/src/CsvFileReader.ts
@@ -1,5 +1,5 @@
 import fs from 'fs';
-
+import { dateStringToDate } from './utils';
 export class CsvFileReader {
   data: string[][] = [];
   constructor(public filename: string) {}
@@ -10,6 +10,15 @@ export class CsvFileReader {
         encoding: 'utf-8',
       })
       .split('\n')
-      .map((row: string): string[] => row.split(','));
+      .map((row: string): string[] => row.split(','))
+      .map((row: string[]): any => {
+        return [
+          dateStringToDate(row[0]),
+          row[1],
+          row[2],
+          parseInt(row[3]),
+          parseInt(row[4]),
+        ];
+      });
   }
 }
