0eb248d45943bad497eba6d167f87f45d71ef094
r0mka <romanliveshere@gmail.com>
2020-11-22 00:56:20 -0800

    Add static method to MatchReader

--- 3-stats/src/MatchReader.ts
+++ 3-stats/src/MatchReader.ts
@@ -1,12 +1,16 @@
 import { dateStringToDate } from './utils';
 import { MatchData } from './MatchData';
 import { MatchResult } from './MatchResult';
+import { CsvFileReader } from './CsvFileReader';
 interface DataReader {
   read(): void;
   data: string[][];
 }
 
 export class MatchReader {
+  static fromCsv(filename: string): MatchReader {
+    return new MatchReader(new CsvFileReader(filename));
+  }
   matches: MatchData[] = [];
   constructor(public reader: DataReader) {}
 
--- 3-stats/src/index.ts
+++ 3-stats/src/index.ts
@@ -1,13 +1,7 @@
 import { MatchReader } from './MatchReader';
-import { CsvFileReader } from './CsvFileReader';
 import { Summary } from './Summary';
 
-// Create an object that satisfies the DataReader interface
-const csvFileReader = new CsvFileReader('football.csv');
-// Create an instance of MatchReader and pass in something satisfying
-// the 'DataReader' interface
-const matchReader = new MatchReader(csvFileReader);
-matchReader.load();
-
+const matchReader = MatchReader.fromCsv('football.csv');
 const summary = Summary.winAnalysisWithHtmlReport('Man United');
+matchReader.load();
 summary.buildAndPrintReport(matchReader.matches);
