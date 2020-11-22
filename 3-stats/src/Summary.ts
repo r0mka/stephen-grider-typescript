ae804c36b4b3f20613997637d1eb32e5ab0f4290
r0mka <romanliveshere@gmail.com>
2020-11-22 00:44:45 -0800

    Static method on Summary

--- 3-stats/src/Summary.ts
+++ 3-stats/src/Summary.ts
@@ -1,5 +1,7 @@
 import { MatchData } from './MatchData';
-
+import { WinsAnalysis } from './analyzers/WinsAnalysis';
+import { HtmlReport } from './reportTargets/HtmlReport';
+import { ConsoleReport } from './reportTargets/ConsoleReport';
 export interface Analyzer {
   run(matches: MatchData[]): string;
 }
@@ -9,8 +11,22 @@ export interface OutputTarget {
 }
 
 export class Summary {
+  static winAnalysisWithHtmlReport(team: string): Summary {
+    return new Summary(
+      new WinsAnalysis(team),
+      new ConsoleReport(),
+      new HtmlReport()
+    );
+  }
   constructor(
     public analyzer: Analyzer,
-    public outputTarget: OutputTarget
+    public outputTarget: OutputTarget,
+    public htmlReport: OutputTarget
   ) {}
+
+  buildAndPrintReport(matches: MatchData[]): void {
+    const output = this.analyzer.run(matches);
+    this.outputTarget.print(output);
+    this.htmlReport.print(output);
+  }
 }
