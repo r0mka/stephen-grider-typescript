05f199c75c2043b39d0c9d503a727a15dc3cf6a3
r0mka <romanliveshere@gmail.com>
2020-11-23 05:15:29 -0800

    implement set method on User class

--- 4-web/src/index.ts
+++ 4-web/src/index.ts
@@ -3,3 +3,7 @@ import { User } from './models/User';
 const user = new User({ name: 'myname', age: 20 });
 console.log(user.get('name'));
 console.log(user.get('age'));
+
+user.set({ name: 'Alex' });
+console.log(user.get('name'));
+console.log(user.get('age'));
