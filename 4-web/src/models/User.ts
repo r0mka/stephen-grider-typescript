d6d7812039603a3b90738b2432038e116eb2bcec
r0mka <romanliveshere@gmail.com>
2020-11-23 05:43:56 -0800

    Implement on method on User class

--- 4-web/src/models/User.ts
+++ 4-web/src/models/User.ts
@@ -3,12 +3,21 @@ interface UserProps {
   age?: number;
 }
 
+type Callback = () => void;
+
 export class User {
+  events: { [key: string]: Callback[] } = {};
   constructor(private data: UserProps) {}
   get(propName: string): number | string {
     return this.data[propName];
   }
   set(update: UserProps): void {
     Object.assign(this.data, update);
+  }
+
+  on(eventName: string, callback: Callback): void {
+    const handlers = this.events[eventName] || [];
+    handlers.push(callback);
+    this.events[eventName] = handlers;
   }
 }
