5874fa6af58ec6b7dc9ffa84385220fff026db47
r0mka <romanliveshere@gmail.com>
2020-11-24 02:09:44 -0800

    Implement fetch method on User class

--- 4-web/src/index.ts
+++ 4-web/src/index.ts
@@ -1,12 +1,9 @@
 import { User } from './models/User';
 
-const user = new User({ name: 'myname', age: 20 });
+const user = new User({ id: 1 });
+user.fetch();
 
-user.on('change', () => {
-  console.log('Change #1');
-});
-user.on('change', () => {
-  console.log('Change #2');
-});
-console.log(user);
-user.trigger('change');
+setTimeout(() => {
+  console.log(user);
+  console.log(user.get('name'));
+}, 4000);
