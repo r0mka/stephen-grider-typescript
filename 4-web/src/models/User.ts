669eb9d53e67c51c3771c8b6194f7ecd70831da9
r0mka <romanliveshere@gmail.com>
2020-11-25 01:31:34 -0800

    Create Eventing Class and move event methods there from User class

--- 4-web/src/models/User.ts
+++ 4-web/src/models/User.ts
@@ -1,36 +1,31 @@
 import axios, { AxiosResponse } from 'axios';
+import { Eventing } from './Eventing';
 interface UserProps {
   id?: number;
   name?: string;
   age?: number;
 }
+// Option #1
+// Accepting dependencies as second constructor argument
 
-type Callback = () => void;
+// Option #2
+// Only accept dependencies into constructor
+// Define a static class method to preconfigure
+// User and assign properties afterwards
+
+// Option #3
+// Only accept properties into constructor
+// Hard code dependencies as class properties
 
 export class User {
-  events: { [key: string]: Callback[] } = {};
+  public events: Eventing = new Eventing();
+
   constructor(private data: UserProps) {}
   get(propName: string): number | string {
     return this.data[propName];
   }
   set(update: UserProps): void {
     Object.assign(this.data, update);
-  }
-
-  on(eventName: string, callback: Callback): void {
-    const handlers = this.events[eventName] || [];
-    handlers.push(callback);
-    this.events[eventName] = handlers;
-  }
-  trigger(eventName: string) {
-    const handlers = this.events[eventName];
-
-    if (!handlers || handlers.length === 0) {
-      return;
-    }
-    handlers.forEach((callback) => {
-      callback();
-    });
   }
 
   fetch(): void {
