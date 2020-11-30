6954db2893e6791131b80084d7e6543da3e02995
r0mka <romanliveshere@gmail.com>
2020-11-30 01:25:59 -0800

    Make View class generic

--- 4-web/src/views/View.ts
+++ 4-web/src/views/View.ts
@@ -1,7 +1,7 @@
-import { User } from '../models/User';
+import { Model } from '../models/Model';
 
-export abstract class View {
-  constructor(public parent: HTMLElement, public model: User) {
+export abstract class View<T extends Model<K>, K> {
+  constructor(public parent: HTMLElement, public model: T) {
     this.bindModel();
   }
   abstract eventsMap(): { [key: string]: () => void };
