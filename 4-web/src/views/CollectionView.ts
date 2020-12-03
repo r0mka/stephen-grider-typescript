315e606bb0338aba87d55d350eda29103e65bc48
r0mka <romanliveshere@gmail.com>
2020-12-03 05:02:24 -0800

    Set up express

--- 4-web/src/views/CollectionView.ts
+++ 4-web/src/views/CollectionView.ts
@@ -1,7 +1,10 @@
 import { Collection } from '../models/Collection';
 
 export abstract class CollectionView<T, K> {
-  constructor(public parent: Element, public collection: Collection<T, K>) {}
+  constructor(
+    public parent: Element,
+    public collection: Collection<T, K>
+  ) {}
 
   abstract renderItem(model: T, itemParent: Element): void;
 
