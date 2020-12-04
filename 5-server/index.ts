39c8231afca597e5ad0ea5c63eefbbbd6f81482b
r0mka <romanliveshere@gmail.com>
2020-12-04 02:59:12 -0800

    type file

--- 5-server/index.ts
+++ 5-server/index.ts
@@ -1,7 +1,10 @@
 import express from 'express';
 import { router } from './src/routes/loginRoutes';
+import bodyParser from 'body-parser';
 
 const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+
 app.use(router);
 
 app.listen(3000, () => console.log('LISTENING ON PORT 3000'));
--- 5-server/src/routes/loginRoutes.ts
+++ 5-server/src/routes/loginRoutes.ts
@@ -7,17 +7,24 @@ router.get('/login', (req: Request, res: Response) => {
     <form method="POST">
      <div>
         <label>Email</label>
-        <input name="email"/>
+        <input name="em"/>
      </div>
      <div>
         <label>Password</label>
-        <input name="password" type="password"/>
+        <input name="pa" type="password"/>
      </div>
      <button>Submit</button>
     </form>
   `);
 });
 
-router.post('/login', (req: Request, res: Response) => {});
+router.post('/login', (req: Request, res: Response) => {
+  const { email, password } = req.body;
+  if (email) {
+    res.send(email.toUpperCase());
+  } else {
+    res.status(400).send('No email');
+  }
+});
 
 export { router };
