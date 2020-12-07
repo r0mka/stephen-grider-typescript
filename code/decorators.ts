f71dcd38341993b69e9945bd15213ec2e97db52e
r0mka <romanliveshere@gmail.com>
2020-12-07 06:40:33 -0800

    decorator factory

--- code/decorators.ts
+++ code/decorators.ts
@@ -5,26 +5,27 @@ class Boat {
     return `this boats color is ${this.color}`;
   }
 
-  @logError
+  @logError('Oops, boat was sunk in ocean')
   pilot(): void {
     throw new Error();
     console.log('swish');
   }
 }
+function logError(errorMessage: string) {
+  return function (
+    target: any,
+    key: string,
+    desc: PropertyDescriptor
+  ): void {
+    const method = desc.value;
 
-function logError(
-  target: any,
-  key: string,
-  desc: PropertyDescriptor
-): void {
-  const method = desc.value;
-
-  desc.value = function () {
-    try {
-      method();
-    } catch (e) {
-      console.log('OOPS BOAT WAS SUNK');
-    }
+    desc.value = function () {
+      try {
+        method();
+      } catch (e) {
+        console.log(errorMessage);
+      }
+    };
   };
 }
 
