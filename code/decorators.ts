38b8e4fa55cc0cd1d6a113c3c77887dd7cf0ab1a
r0mka <romanliveshere@gmail.com>
2020-12-07 00:49:56 -0800

    decorators

---
+++ code/decorators.ts
@@ -0,0 +1,17 @@
+class Boat {
+  color: string = 'red';
+
+  get formattedColor(): string {
+    return `this boats color is ${this.color}`;
+  }
+
+  @logError
+  pilot(): void {
+    throw new Error();
+  }
+}
+
+function logError(target: any, key: string): void {
+  console.log('Target: ', target);
+  console.log('Key: ', key);
+}
