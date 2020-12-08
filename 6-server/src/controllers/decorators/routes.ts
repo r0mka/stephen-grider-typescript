55e4789e52fe78a52cecb9d92083de3a411d60e6
r0mka <romanliveshere@gmail.com>
2020-12-08 02:57:35 -0800

    MetaDataKeys

--- 6-server/src/controllers/decorators/routes.ts
+++ 6-server/src/controllers/decorators/routes.ts
@@ -1,4 +1,6 @@
 import 'reflect-metadata';
+import { Methods } from './Methods';
+import { MetaDataKeys } from './MetadataKeys';
 
 function routeBinder(method: string) {
   return function (path: string) {
@@ -7,14 +9,19 @@ function routeBinder(method: string) {
       key: string,
       desc: PropertyDescriptor
     ) {
-      Reflect.defineMetadata('path', path, target, key);
-      Reflect.defineMetadata('method', method, target, key);
+      Reflect.defineMetadata(MetaDataKeys.path, path, target, key);
+      Reflect.defineMetadata(
+        MetaDataKeys.method,
+        method,
+        target,
+        key
+      );
     };
   };
 }
 
-export const get = routeBinder('get');
-export const put = routeBinder('put');
-export const post = routeBinder('post');
-export const del = routeBinder('delete');
-export const patch = routeBinder('patch');
+export const get = routeBinder(Methods.get);
+export const put = routeBinder(Methods.put);
+export const post = routeBinder(Methods.post);
+export const del = routeBinder(Methods.del);
+export const patch = routeBinder(Methods.patch);
