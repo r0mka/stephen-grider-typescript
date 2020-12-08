9d3975f20ccdf65f2067c49610f6e534de196c79
r0mka <romanliveshere@gmail.com>
2020-12-08 01:50:18 -0800

    AppRouter

--- 6-server/src/index.ts
+++ 6-server/src/index.ts
@@ -2,12 +2,13 @@ import express from 'express';
 import { router } from './routes/loginRoutes';
 import bodyParser from 'body-parser';
 import cookieSession from 'cookie-session';
-import { router as controllerRouter } from './controllers/decorators/controller';
+import { AppRouter } from './AppRouter';
+
 import './controllers/loginController';
 
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieSession({ keys: ['laskdjf'] }));
 app.use(router);
-app.use(controllerRouter);
+app.use(AppRouter.getInstance());
 app.listen(3000, () => console.log('LISTENING ON PORT 3000'));
