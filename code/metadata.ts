afd0da2e6b81aa1df7ae47bdb88415143ff844e8
r0mka <romanliveshere@gmail.com>
2020-12-08 00:32:35 -0800

    decorators on a class

--- code/metadata.ts
+++ code/metadata.ts
@@ -1,8 +1,8 @@
 import 'reflect-metadata';
 
-const plane = {
-  color: 'red',
-};
+// const plane = {
+//   color: 'red',
+// };
 
 // Reflect.defineMetadata('note', 'hi there', plane);
 // Reflect.defineMetadata('height', 10, plane);
@@ -13,6 +13,36 @@ const plane = {
 // console.log(note);
 // console.log(height);
 
-Reflect.defineMetadata('note', 'hi there', plane, 'color');
-const note = Reflect.getMetadata('note', plane, 'color');
-console.log(note);
+// Reflect.defineMetadata('note', 'hi there', plane, 'color');
+// const note = Reflect.getMetadata('note', plane, 'color');
+// console.log(note);
+
+@printMetaData
+class Plane {
+  color: string = 'red';
+
+  @markFunction('hi there')
+  fly(): void {
+    console.log('vrrrrrr');
+  }
+}
+
+function markFunction(secretInfo: string) {
+  return function (target: Plane, key: string) {
+    Reflect.defineMetadata('secret', secretInfo, target, key);
+  };
+}
+
+function printMetaData(target: typeof Plane) {
+  for (let key in target.prototype) {
+    const secret = Reflect.getMetadata(
+      'secret',
+      target.prototype,
+      key
+    );
+    console.log(secret);
+  }
+}
+
+// const secret = Reflect.getMetadata('secret', Plane.prototype, 'fly');
+// console.log(secret);
