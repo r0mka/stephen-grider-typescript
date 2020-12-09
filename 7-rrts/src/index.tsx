bf83d4295a0d459e6a6e9b723ba13ae350711835
r0mka <romanliveshere@gmail.com>
2020-12-09 04:24:29 -0800

    React setup

--- 7-rrts/src/index.tsx
+++ 7-rrts/src/index.tsx
@@ -3,14 +3,16 @@ import ReactDOM from 'react-dom';
 import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
-import { App } from './components/App';
-import { reducers } from './reducers';
+// import { App } from './components/App';
+// import { reducers } from './reducers';
 
-const store = createStore(reducers, applyMiddleware(thunk));
+interface AppProps {
+  color?: string;
+}
+class App extends React.Component<AppProps> {
+  render() {
+    return <div>{this.props.color}</div>;
+  }
+}
 
-ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
-  document.querySelector('#root')
-);
+ReactDOM.render(<App />, document.querySelector('#root'));
