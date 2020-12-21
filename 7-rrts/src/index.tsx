d86d695467a7b7e382179703a9cfdedde072fe68
r0mka <romanliveshere@gmail.com>
2020-12-21 00:22:25 -0800

    app functional component

--- 7-rrts/src/index.tsx
+++ 7-rrts/src/index.tsx
@@ -9,10 +9,44 @@ import thunk from 'redux-thunk';
 interface AppProps {
   color?: string;
 }
-class App extends React.Component<AppProps> {
-  render() {
-    return <div>{this.props.color}</div>;
-  }
-}
+
+const App = (props: AppProps): JSX.Element => {
+  const [counter, setCounter] = React.useState(1);
+
+  const onIncrement = (): void => {
+    setCounter(counter + 1);
+  };
+  const onDecrement = (): void => {
+    setCounter(counter - 1);
+  };
+
+  return (
+    <div>
+      <button onClick={onIncrement}>Increment</button>
+      <span>{counter}</span>
+      <button onClick={onDecrement}>Decrement</button>
+    </div>
+  );
+};
+
+// class App extends React.Component<AppProps> {
+//   state = { counter: 1 };
+
+//   onIncrement = (): void => {
+//     this.setState({ counter: this.state.counter + 1 });
+//   };
+//   onDecrement = (): void => {
+//     this.setState({ counter: this.state.counter - 1 });
+//   };
+//   render() {
+//     return (
+//       <div>
+//         <button onClick={this.onIncrement}>Increment</button>
+//         <span>{this.state.counter}</span>
+//         <button onClick={this.onDecrement}>Decrement</button>
+//       </div>
+//     );
+//   }
+// }
 
 ReactDOM.render(<App />, document.querySelector('#root'));
