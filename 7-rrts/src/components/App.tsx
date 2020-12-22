63f2fe75c60687ddc9358c86019c8a6229054929
r0mka <romanliveshere@gmail.com>
2020-12-22 02:14:58 -0800

    fetch todos from App

--- 7-rrts/src/components/App.tsx
+++ 7-rrts/src/components/App.tsx
@@ -1,7 +1,42 @@
 import React from 'react';
 import axios, { AxiosResponse, AxiosError } from 'axios';
-export class App extends React.Component {
+import { connect } from 'react-redux';
+import { Todo, fetchTodos } from '../actions';
+import { StoreState } from '../reducers';
+
+interface AppProps {
+  todos: Todo[];
+  fetchTodos(): any;
+}
+class _App extends React.Component<AppProps> {
+  onButtonClick = (): void => {
+    this.props.fetchTodos();
+  };
+
+  renderList(): JSX.Element[] {
+    return this.props.todos.map((todo: Todo) => {
+      return (
+        <div key={todo.id}>
+          {todo.title}
+          <button>Delete</button>
+        </div>
+      );
+    });
+  }
+
   render() {
-    return <div>Hi there!</div>;
+    return (
+      <div>
+        <button onClick={this.onButtonClick}>Fetch</button>
+        {this.renderList()}
+      </div>
+    );
   }
 }
+const mapStateToProps = ({
+  todos,
+}: StoreState): { todos: Todo[] } => ({
+  todos,
+});
+
+export const App = connect(mapStateToProps, { fetchTodos })(_App);
