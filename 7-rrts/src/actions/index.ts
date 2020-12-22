384fd04eca145109f35bbb024f559df275293326
r0mka <romanliveshere@gmail.com>
2020-12-22 00:17:17 -0800

    interface for an action creator

--- 7-rrts/src/actions/index.ts
+++ 7-rrts/src/actions/index.ts
@@ -1,13 +1,26 @@
 import axios, { AxiosResponse, AxiosError } from 'axios';
 import { Dispatch } from 'redux';
+import { ActionTypes } from './types';
 
 const url = 'https://jsonplaceholder.typicode.com/todos';
 
+interface Todo {
+  id: number;
+  title: string;
+  completed: boolean;
+}
+
+interface FetchTodosAction {
+  type: ActionTypes.fetchTodos;
+  payload: Todo[];
+}
+
 export const fetchTodos = () => {
   return async (dispatch: Dispatch) => {
-    const response = await axios.get(url);
-    dispatch({
-      type: 'FETCH_TODOS',
+    const response = await axios.get<Todo[]>(url);
+
+    dispatch<FetchTodosAction>({
+      type: ActionTypes.fetchTodos,
       payload: response.data,
     });
   };
