2dee7e4c21ad91fac4c488bc9b7457213e04d2ab
r0mka <romanliveshere@gmail.com>
2020-12-24 19:56:48 -0800

    Add deleteTodologic

--- 7-rrts/src/components/App.tsx
+++ 7-rrts/src/components/App.tsx
@@ -1,24 +1,27 @@
 import React from 'react';
 import axios, { AxiosResponse, AxiosError } from 'axios';
 import { connect } from 'react-redux';
-import { Todo, fetchTodos } from '../actions';
+import { Todo, fetchTodos, deleteTodo } from '../actions';
 import { StoreState } from '../reducers';
 
 interface AppProps {
   todos: Todo[];
-  fetchTodos(): any;
+  fetchTodos: typeof fetchTodos;
+  deleteTodo: typeof deleteTodo;
 }
 class _App extends React.Component<AppProps> {
   onButtonClick = (): void => {
     this.props.fetchTodos();
   };
+  onTodoClick = (id: number): void => {
+    this.props.deleteTodo(id);
+  };
 
   renderList(): JSX.Element[] {
     return this.props.todos.map((todo: Todo) => {
       return (
-        <div key={todo.id}>
+        <div onClick={() => this.onTodoClick(todo.id)} key={todo.id}>
           {todo.title}
-          <button>Delete</button>
         </div>
       );
     });
@@ -39,4 +42,7 @@ const mapStateToProps = ({
   todos,
 });
 
-export const App = connect(mapStateToProps, { fetchTodos })(_App);
+export const App = connect(mapStateToProps, {
+  fetchTodos,
+  deleteTodo,
+})(_App);
